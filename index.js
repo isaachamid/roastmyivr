8dee77c909cfa0fb683ed41d728c951c46e693c7
hamidisaac <isaac.hamid@gmail.com>
2023-06-12 13:13:25 +0330

    start roasting api

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+require('dotenv').config()
 const express = require('express');
+const axios = require('axios');
 const app = express();
-const port = 8000;
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Test API!'));
+
+// add ivr and start roasting and save user data
+app.post('/ivr/start', async (req, res) => {
+    const apiPath = `${process.env.BASE_API}/portal/ivrs`;
+    const config = {
+        headers: {
+            'x-api-key': process.env.ROAST_API_KEY,
+            'x-secret-key': process.env.ROAST_SECRET_KEY
+        }
+    }
+    axios.post(apiPath, req.body, config)
+        .then((response) => res.json(response.data))
+        .catch((err) => console.log(err));
+});
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Listening on port ' + port);
+    console.log(`API server listening on port ${port}`);
 });
